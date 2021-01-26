79f388f47c1ae5bdd863350e209a96f14e6a1c8a
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-01-26 16:08:05 -0300

    Manipulação de elementos

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-var d = new Date();
-alert(d.getDay());
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>"
+}
+function redirecionar(){
+    window.open("https://www.google.com/")
+    window.location.href="https://www.google.com/";
+}
+function msov(elemento){
+    elemento.innerHTML="Obrigado por passar o mouse!";
+}
+function voltar(elemento){
+    elemento.innerHTML="Passe o mouse aqui!";
+}
+function load(){
+    alert("Página carregada")
+}
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
