9174cd3cd926aee3b92d617f2e5ba7590493851e
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-01-26 15:20:08 -0300

    Arrays e repetição

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
-var nome = "Rafael Ritzmann Resnauer";
-var n1 = 22;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo!"
-//alert(nome + " tem " + idade + " anos");
-//alert(idade + idade2)
-console.log(nome)
-console.log(n1*n2)
-console.log(frase.toLowerCase())
+var d = new Date();
+alert(d.getDay());
